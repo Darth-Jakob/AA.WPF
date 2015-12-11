ef41f7ecef5f719675924072a1745c8fe7f610ec
JakobSung <alynarasung@gmail.com>
2015-12-11 11:32:41 +0900

    Assembly info 수정

--- AA.WPF.MVVM/Properties/AssemblyInfo.cs
+++ AA.WPF.MVVM/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AA.WPF.MVVM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LG")]
+[assembly: AssemblyCompany("JAKOB")]
 [assembly: AssemblyProduct("AA.WPF.MVVM")]
-[assembly: AssemblyCopyright("Copyright © LG 2015")]
+[assembly: AssemblyCopyright("Copyright © JAKOB 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
