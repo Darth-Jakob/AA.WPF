f618a46b20a52d1716f3acd05e214865bc3e0b4f
JakobSung <aajakobsung@gmail.com>
2019-05-21 10:29:19 +0900

    control add
    Drag&drop listbox
    imageButton

---
+++ AA.WPF.Controls/Control/DDListbox.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MCPTT_Dispatcher
+{
+    /// <summary>
+    /// drag & drop 구현한 listbox
+    /// </summary>
+    public class DDListbox : ListBox
+    {
+        public static RoutedEvent DropItemMoveEvent = EventManager.RegisterRoutedEvent("DropItemMove", RoutingStrategy.Bubble, typeof(DropItemMoveEventArgs), typeof(DDListbox));
+        /// <summary>
+        /// drag item 순서 변경 이벤트
+        /// </summary>
+        public event RoutedEventHandler DropItemMove
+        {
+            add { AddHandler(DropItemMoveEvent, value); }
+            remove { RemoveHandler(DropItemMoveEvent, value); }
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new DDListboxItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is DDListboxItem;
+        }
+
+        /// <summary>
+        /// drop으로 item move
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="dDListboxItem"></param>
+        internal void DropMoveItem(DDListboxItem source, DDListboxItem target)
+        {
+            //이벤트 발생
+            RaiseEvent(new DropItemMoveEventArgs(DropItemMoveEvent) { DargSource = source, DropTarget = target });
+
+            //ItemsSource 사용안할시 내부에서 직접 순서 변경함. 
+            if (ItemsSource == null)
+            {
+                int idx = ItemContainerGenerator.IndexFromContainer(target);
+                var sourceData = ItemContainerGenerator.ItemFromContainer(source);
+                Items.Remove(sourceData);
+                Items.Insert(idx, sourceData);
+            }
+        }
+    }
+
+    /// <summary>
+    /// drag 순서 변경 RoutedEventArgs
+    /// </summary>
+    public class DropItemMoveEventArgs : RoutedEventArgs
+    {
+        public DropItemMoveEventArgs(RoutedEvent routedEvent) : base(routedEvent)
+        {
+        }
+
+        /// <summary>
+        /// drag object
+        /// </summary>
+        internal object DargSource { get; set; }
+        /// <summary>
+        /// drop target
+        /// </summary>
+        internal object DropTarget { get; set; }
+    }
+    /// <summary>
+    /// drag & drop 구현한 listboxItem
+    /// </summary>
+    public class DDListboxItem : ListBoxItem
+    {
+        public static readonly DependencyProperty IsDragOverProperty = DependencyProperty.Register("IsDragOver", typeof(bool), typeof(DDListboxItem));
+        public bool IsDragOver
+        {
+            get { return (bool)GetValue(IsDragOverProperty); }
+            set { SetValue(IsDragOverProperty, value); }
+        }
+
+
+        Point _moveMousePoint;
+        Point _startMousePoint;
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                _startMousePoint = e.GetPosition(this);
+            }
+
+            base.OnPreviewMouseLeftButtonDown(e);
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            base.OnPreviewMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                //마우스 다운이 되지않아 일정 간격 이동시에 drag로 간주한다. 일단 15픽셀 이동시
+                _moveMousePoint = e.GetPosition(this);
+                if (Math.Abs(_startMousePoint.X - _moveMousePoint.X) > 15 || Math.Abs(_startMousePoint.Y - _moveMousePoint.Y) > 15)
+                {
+                    //drag 시작
+                    DataObject dragData = new DataObject("ChannelItem", this);
+                    DragDrop.DoDragDrop(this, dragData, DragDropEffects.Move);
+                }
+            }
+        }
+
+        protected override void OnPreviewDrop(DragEventArgs e)
+        {
+            IsDragOver = false;
+            base.OnPreviewDrop(e);
+
+            //dragdata 검증
+            if (e.Data.GetDataPresent("ChannelItem"))
+            {
+                var dragSource = e.Data.GetData("ChannelItem");
+
+                var owner = ItemsControl.ItemsControlFromItemContainer(this);
+                if (owner is DDListbox)
+                {
+                    //부모 itemcontrol 함수 호출
+                    ((DDListbox)owner).DropMoveItem(dragSource as DDListboxItem, this);
+                }
+            }
+        }
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            base.OnDragOver(e);
+        }
+
+        protected override void OnDragEnter(DragEventArgs e)
+        {
+            base.OnDragEnter(e);
+            if (e.Data.GetDataPresent("ChannelItem"))
+            {
+                IsDragOver = true;
+            }
+        }
+
+        protected override void OnDragLeave(DragEventArgs e)
+        {
+            IsDragOver = false;
+            base.OnDragLeave(e);
+        }
+    }
+}
---
+++ AA.WPF.Controls/Control/ImageButton.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MCPTT_Dispatcher
+{
+    [TemplatePart(Name = "PART_TXT", Type = typeof(TextBlock))]
+    /// <summary>
+    /// Image가 들어간 커스텀 버튼
+    /// </summary>
+    public class ImageButton : Button
+    {
+        #region constructor
+        static ImageButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton), new FrameworkPropertyMetadata(typeof(ImageButton)));
+        }
+        #endregion
+
+        #region Dependency property
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(ImageButton), new PropertyMetadata(string.Empty));
+        /// <summary>
+        /// 기본이미지소스
+        /// </summary>
+        public static readonly DependencyProperty ImageDefaultProperty = DependencyProperty.Register("ImageDefault", typeof(ImageSource), typeof(ImageButton));
+        /// <summary>
+        /// 마우스오버 이미지소스
+        /// </summary>
+        public static readonly DependencyProperty ImageOverProperty = DependencyProperty.Register("ImageOver", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(new PropertyChangedCallback(OnChangedOver)));
+
+        private static void OnChangedOver(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ImageButton control = d as ImageButton;
+            var s = e.NewValue as ImageSource;
+            Debug.WriteLine(s);
+        }
+
+        /// <summary>
+        /// 비활성화 이미지소스
+        /// </summary>
+        public static readonly DependencyProperty ImageDisabledProperty = DependencyProperty.Register("ImageDisabled", typeof(ImageSource), typeof(ImageButton));
+        /// <summary>
+        /// pressed 이미지소스
+        /// </summary>
+        public static readonly DependencyProperty ImagePressedProperty = DependencyProperty.Register("ImagePressed", typeof(ImageSource), typeof(ImageButton));
+        /// <summary>
+        /// 마우스 캡쳐 릴리즈 command
+        /// </summary>
+        public static readonly DependencyProperty MouseCaptureReleaseCommandProperty = DependencyProperty.Register("MouseCaptureReleaseCommand", typeof(ICommand), typeof(ImageButton));
+        /// <summary>
+        /// 마우스 캡쳐 command
+        /// </summary>
+        public static readonly DependencyProperty MouseCaptureComamndProperty = DependencyProperty.Register("MouseCaptureComamnd", typeof(ICommand), typeof(ImageButton));
+
+        public static readonly DependencyProperty IsMouseCapturingProperty = DependencyProperty.Register("IsMouseCapturing", typeof(bool), typeof(ImageButton));
+
+        public static readonly DependencyProperty TextLocationProperty = DependencyProperty.Register("TextLocation", typeof(Enums.ImageButtonTextLocation), typeof(ImageButton), new PropertyMetadata(Enums.ImageButtonTextLocation.Right));
+
+        public static readonly DependencyProperty ForegroundDisableProperty = DependencyProperty.Register("ForegroundDisable", typeof(Brush), typeof(ImageButton), new PropertyMetadata((SolidColorBrush)(new BrushConverter().ConvertFrom("#FF3d434d"))));
+
+        public static readonly DependencyProperty ForegroundMouseOverProperty = DependencyProperty.Register("ForegroundMouseOver", typeof(Brush), typeof(ImageButton), new PropertyMetadata((SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFFFFFF"))));
+
+        public static readonly DependencyProperty ForegroundPressedProperty = DependencyProperty.Register("ForegroundPressed", typeof(Brush), typeof(ImageButton), new PropertyMetadata((SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFFFFFF"))));
+
+        public static readonly DependencyProperty TextMarginProperty = DependencyProperty.Register("TextMargin", typeof(double), typeof(ImageButton), new PropertyMetadata((double)6));
+
+        #endregion
+
+        #region private func
+
+        #endregion
+
+        #region property
+
+        public double TextMargin
+        {
+            get { return (double)GetValue(TextMarginProperty); }
+            set { SetValue(TextMarginProperty, value); }
+        }
+
+        public Brush ForegroundDisable
+        {
+            get { return GetValue(ForegroundDisableProperty) as Brush; }
+            set { SetValue(ForegroundDisableProperty, value); }
+        }
+
+        public Brush ForegroundMouseOver
+        {
+            get { return GetValue(ForegroundMouseOverProperty) as Brush; }
+            set { SetValue(ForegroundMouseOverProperty, value); }
+        }
+
+        public Brush ForegroundPressed
+        {
+            get { return GetValue(ForegroundPressedProperty) as Brush; }
+            set { SetValue(ForegroundPressedProperty, value); }
+        }
+
+        public Enums.ImageButtonTextLocation TextLocation
+        {
+            get { return (Enums.ImageButtonTextLocation)GetValue(TextLocationProperty); }
+            set { SetValue(TextLocationProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return GetValue(TextProperty) as string; }
+            set { SetValue(TextProperty, value); }
+        }
+        public ImageSource ImageDefault
+        {
+            get { return GetValue(ImageDefaultProperty) as ImageSource; }
+            set { SetValue(ImageDefaultProperty, value); }
+        }
+
+        public ImageSource ImageOver
+        {
+            get { return GetValue(ImageOverProperty) as ImageSource; }
+            set { SetValue(ImageOverProperty, value); }
+        }
+        public ImageSource ImageDisabled
+        {
+            get { return GetValue(ImageDisabledProperty) as ImageSource; }
+            set { SetValue(ImageDisabledProperty, value); }
+        }
+        public ImageSource ImagePressed
+        {
+            get { return GetValue(ImagePressedProperty) as ImageSource; }
+            set { SetValue(ImagePressedProperty, value); }
+        }
+        public ICommand MouseCaptureReleaseCommand
+        {
+            get { return GetValue(MouseCaptureReleaseCommandProperty) as ICommand; }
+            set { SetValue(MouseCaptureReleaseCommandProperty, value); }
+        }
+        public ICommand MouseCaptureComamnd
+        {
+            get { return GetValue(MouseCaptureComamndProperty) as ICommand; }
+            set { SetValue(MouseCaptureComamndProperty, value); }
+
+        }
+
+        public bool IsMouseCapturing
+        {
+            get { return (bool)GetValue(IsMouseCapturingProperty); }
+            set
+            {
+                SetValue(IsMouseCapturingProperty, value);
+            }
+        }
+
+        #endregion
+
+        #region override
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (ImageDefault != null)
+            {
+                if (ImageOver == null)
+                {
+                    ImageOver = ImageDefault;
+                }
+                if (ImageDisabled == null)
+                {
+                    ImageDisabled = ImageDefault;
+                }
+                if (ImagePressed == null)
+                {
+                    ImagePressed = ImageDefault;
+                }
+            }
+
+
+            var txt = GetTemplateChild("PART_TXT") as TextBlock;
+            if (txt != null)
+            {
+                txt.Margin = new Thickness(TextMargin);
+            }
+            else
+            {
+               
+            }
+            
+        }
+
+        protected override void OnGotMouseCapture(MouseEventArgs e)
+        {
+            base.OnGotMouseCapture(e);
+            IsMouseCapturing = true;
+
+            if (MouseCaptureComamnd != null)
+            {
+                MouseCaptureComamnd.Execute(null);
+            }
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+            if (IsMouseCapturing)
+            {
+                IsMouseCapturing = false;
+                //마우스 캡처 끝나면 커맨더 발생시킴.
+                if (MouseCaptureReleaseCommand != null)
+                {
+                    MouseCaptureReleaseCommand.Execute(null);
+                }
+            }
+        }
+        protected override void OnLostMouseCapture(MouseEventArgs e)
+        {
+            base.OnLostMouseCapture(e);
+            
+        }
+        #endregion
+
+    }
+}
